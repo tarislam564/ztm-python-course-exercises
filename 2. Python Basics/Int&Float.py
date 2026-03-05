1f1f1f6fae0bcdb77fb49685b5987d58de4f4a27
tarislam564 <tariqul0091@gmail.com>
2026-03-05 23:26:16 +0600

    Update Int&Float.py

--- 2. Python Basics/Int&Float.py
+++ 2. Python Basics/Int&Float.py
@@ -2,41 +2,63 @@
 # int & float
 
 print(2 + 4)
+6
 print(2 - 4)
+-2
 print(2 * 4)
+8
 print(2 / 4)
+0.5
 print("\n")
 
 print(type(2 + 4))
+<class 'int'>
 print(type(2 - 4))
+<class 'int'>
 print(type(2 * 4))
+<class 'int'>
 print(type(2 / 4))
+<class 'float'>
 print(type(0))
+<class 'int'>
 print("\n")
 
 print(type(20 + 1.1))
+<class 'float'>
 print(type(9.9 + 1.1))
+<class 'float'>
 print(9.9 + 1.1)
+11.0
 print("\n")
 
-print((2 ** 3))     # to the power of
+print((2 ** 3))# to the power of
+8
 print(5 // 3)       # integer division rounded down
+1
 print(5 % 3)        # modulo / remainder
+2
 print("\n")
+
 
 # Maths functions
 # Round
 print(round(3.1))
+3
 print(round(3.9))
-
+4
 # Absolute values
 print(abs(-20))
+20
 print("\n")
+
 
 # Complex Numbers
 complex()
 
 # Binary numbers
 print(bin(5))
+0b101
 print(int("0b101", 2))      # ("number", base of number)
+5
 print("\n")
+
