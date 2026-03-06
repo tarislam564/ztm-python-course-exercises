a1ca027ca75cf3ac32df36712b61a791bb8f4274
tarislam564 <tariqul0091@gmail.com>
2026-03-06 16:06:26 +0600

    Update 4-ExerciseStringIndexes.py

--- 2. Python Basics/4-ExerciseStringIndexes.py
+++ 2. Python Basics/4-ExerciseStringIndexes.py
@@ -1,12 +1,21 @@
 #Guess the output of each print statement before you click RUN!
-python = 'I am PYHTON'
+python = 'I am PYTHON'
 
 print(python[1:4])
+ am
 print(python[1:])
+ am PYTHON
 print(python[:])
+I am PYTHON
 print(python[1:100])
+ am PYTHON
 print(python[-1])
+N
 print(python[-4])
+T
 print(python[:-3])
+I am PYT
 print(python[-3:])
+HON
 print(python[::-1])
+NOHTYP ma I
