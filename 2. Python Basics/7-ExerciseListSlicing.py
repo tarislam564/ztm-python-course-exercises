f4bc284fc9022b45af15cf2ea34d2cdd04c732dc
tarislam564 <tariqul0091@gmail.com>
2026-03-07 10:54:24 +0600

    Update 7-ExerciseListSlicing.py

--- 2. Python Basics/7-ExerciseListSlicing.py
+++ 2. Python Basics/7-ExerciseListSlicing.py
@@ -2,13 +2,19 @@
 #Before you clikc RUN, guess the output of each print statement!
 new_list = ['a', 'b', 'c']
 print(new_list[1])
+b
 print(new_list[-2])
+b
 print(new_list[1:3])
+['b', 'c']
 new_list[0] = 'z'
 print(new_list)
+['z', 'b', 'c']
 
 my_list = [1,2,3]
 bonus = my_list + [5]
 my_list[0] = 'z'
 print(my_list)
 print(bonus)
+['z', 2, 3]
+[1, 2, 3, 5]
