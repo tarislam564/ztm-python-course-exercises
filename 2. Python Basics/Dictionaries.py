f8ed7298992f944f0a90bad0b1490eee5386ba19
tarislam564 <tariqul0091@gmail.com>
2026-03-07 21:25:48 +0600

    Update Dictionaries.py

--- 2. Python Basics/Dictionaries.py
+++ 2. Python Basics/Dictionaries.py
@@ -9,6 +9,8 @@ dictionary = {
 print(dictionary['a'][1])
 print(dictionary)
 print('\n')
+2
+{'a': [1, 2, 3], 'b': 'Hello', 'c': True}
 
 
 # Dictionary Methods
@@ -20,22 +22,32 @@ user = {
 }
 
 print(user.get('age', 55))
-
+20
 user2 = dict(name='JohnJohn')
 print(user2)
-
+{'name': 'JohnJohn'}
 print('basket' in user)
+True
 print('size' in user)
+False
 print('age' in user.keys())
+True
 print('Hello' in user.values())
+True
 print(user.items())
-
+dict_items([('basket', [1, 2, 3]), ('greet', 'Hello'), ('age', 20)])
 user2 = user.copy()
 user.clear()
 print(user)
+{}
 print(user2)
+{'basket': [1, 2, 3], 'greet': 'Hello', 'age': 20}
 print(user2.pop('age'))
+20
 print(user2.popitem())
+('greet', 'Hello')
 user2.update({'age': 55})
+
 print(user2)
+{'basket': [1, 2, 3], 'age': 55}
 print('\n')
