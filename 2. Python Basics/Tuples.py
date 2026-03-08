739a3e0a1a6308ebd3ad8bb9cb837bb807a83fbd
tarislam564 <tariqul0091@gmail.com>
2026-03-08 12:44:11 +0600

    Update Tuples.py

--- 2. Python Basics/Tuples.py
+++ 2. Python Basics/Tuples.py
@@ -2,12 +2,18 @@
 
 my_tuple = (1, 2, 3, 4, 5)
 print(my_tuple[1])
+2
 print(5 in my_tuple)
+True
 print(my_tuple)
+(1, 2, 3, 4, 5)
 
 x, y, z, *other = (1, 2, 3, 4, 5)
 print(x)
+1
 print(z)
-
+3
 print(my_tuple.count(5))
+1
 print(my_tuple.index(5))
+4
