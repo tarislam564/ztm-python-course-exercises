97f9e92e81de910bc5cb6b0b1eeec66222ec0bf6
tarislam564 <tariqul0091@gmail.com>
2026-03-16 14:46:26 +0600

    Update ConditionalLogic.py

--- 3. Python Basics II/ConditionalLogic.py
+++ 3. Python Basics II/ConditionalLogic.py
@@ -9,14 +9,15 @@ elif is_licenced:
     print('You can drive now!')
 else:
     print('You are not of age!')
-
+You are old enough to drive!
 print('ok ok')
 print()
-
+ok ok
 # improved version
 if is_old and is_licenced:
     print('You are old enough to drive, and you have a licence!')
 else:
     print('You are not of age!')
-
+You are old enough to drive, and you have a licence!
 print('ok ok')
+ok ok
