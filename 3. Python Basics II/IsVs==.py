4677458b2f1be5a0937dc074709389bbff1cbdb8
tarislam564 <tariqul0091@gmail.com>
2026-03-22 16:31:28 +0600

    Update IsVs==.py

--- 3. Python Basics II/IsVs==.py
+++ 3. Python Basics II/IsVs==.py
@@ -6,11 +6,19 @@ print([] == 1)
 print(10 == 10.0)
 print([] == [])
 print()
+True
+False
+False
+True
+True
 
 print(True == True)
 print('1' == 1)
 print([1, 2, 3] == [1, 2, 3])
 print()
+True
+False
+True
 
 print(True is 1)
 print('1' is 1)
@@ -18,7 +26,15 @@ print([] is 1)
 print(10 is 10.0)
 print([1, 2, 3] is [1, 2, 3])
 print()
+False
+False
+False
+False
+False
 
 print(True is True)
 print('1' is '1')
 print([] is [])
+True
+True
+False
