49d8679d1561fa08bfd74cc8afc1bafc122710d2
tarislam564 <tariqul0091@gmail.com>
2026-03-26 13:03:58 +0600

    Update Iterables.py

--- 3. Python Basics II/Iterables.py
+++ 3. Python Basics II/Iterables.py
@@ -8,15 +8,26 @@ user = {
 
 for item in user:
     print(item)
-
+name
+age
+can_swim
 for item in user.items():
     print(item)
-
+('name', 'Golem')
+('age', 5006)
+('can_swim', False)
 for item in user.keys():
     print(item)
-
+name
+age
+can_swim
 for item in user.values():
     print(item)
-
+Golem
+5006
+False
 for key, value in user.items():
     print(key, value)
+name Golem
+age 5006
+can_swim False
