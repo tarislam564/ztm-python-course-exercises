c79b2d67d7d554c8203b0e21997619a1c87f2a07
tarislam564 <tariqul0091@gmail.com>
2026-03-27 10:39:45 +0600

    Update Enumerate().py

--- 3. Python Basics II/Enumerate().py
+++ 3. Python Basics II/Enumerate().py
@@ -2,10 +2,20 @@
 
 for i, char in enumerate('Helllooo'):
     print(i, char)
-
+0 H
+1 e
+2 l
+3 l
+4 l
+5 o
+6 o
+7 o
 for i, char in enumerate([1, 2, 3]):
     print(i, char)
-
+0 1
+1 2
+2 3
 for i, char in enumerate(range(100)):
     if char == 50:
         print(f"index of 50 is: {i}")
+index of 50 is: 50
