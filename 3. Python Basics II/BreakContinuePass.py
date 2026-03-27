0604fdf0145014f3814d3087749073479bde2a9e
tarislam564 <tariqul0091@gmail.com>
2026-03-27 19:52:42 +0600

    Update BreakContinuePass.py

--- 3. Python Basics II/BreakContinuePass.py
+++ 3. Python Basics II/BreakContinuePass.py
@@ -7,17 +7,20 @@ while i < len(my_list):
     i += 1
     continue
 print()
-
+1
+2
+3
 i = 0
 while i < len(my_list):
     print(my_list[i])
     i += 1
     break
 print()
-
+1
 i = 0
 while i < len(my_list):
     i += 1
     pass
 
 print("No error")
+No error
