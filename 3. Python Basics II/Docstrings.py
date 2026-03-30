f4a2ae265a4854bc1f6ef9a64b01f18f3dacc233
tarislam564 <tariqul0091@gmail.com>
2026-03-30 16:24:52 +0600

    Update Docstrings.py

--- 3. Python Basics II/Docstrings.py
+++ 3. Python Basics II/Docstrings.py
@@ -8,10 +8,18 @@ def test(a):
     """
 
     print(a)
-
+Nothing
 
 test('!!!')
-
+!!!
 help(test)
 print()
 print(test.__doc__)
+Help on function test in module __main__:
+test(a)
+    Info : This function tests and prints param a
+    :param a:
+    :return:
+Info : This function tests and prints param a
+:param a:
+:return:
