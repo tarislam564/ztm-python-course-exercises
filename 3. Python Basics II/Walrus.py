139f73e69578b3813ef6924377052b722d79a97f
tarislam564 <tariqul0091@gmail.com>
2026-03-31 11:52:22 +0600

    Update Walrus.py

--- 3. Python Basics II/Walrus.py
+++ 3. Python Basics II/Walrus.py
@@ -2,9 +2,23 @@ a = 'helloooooooooo'
 
 if ((n := len(a)) > 10):
     print(f"too long {n} elements")
-
+too long 14 elements
 while ((n := len(a)) > 1):
     print(n)
     a = a[:-1]
 
 print(a)
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+h
